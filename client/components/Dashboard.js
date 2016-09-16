e2d3b00245ce6a525ac4a538f1563c0cddec54dc
edsant1 <edwins10329@gmail.com>
2016-09-15 20:20:51 -0600

    first commit, adding in the auth, almost finish

---
+++ client/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dashboard = () => (
+	<div>
+		<h2>Welcome to your Dashboard</h2>
+	</div>
+
+);
+
+export default Dashboard;
--- client/routes.js
+++ client/routes.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import NotFound from './components/NotFound';
+import Dashboard from './components/Dashboard';
+import { UserAuthWrapper } from 'redux-auth-wrapper';
+
+const UserIsAuthenticated = UserAuthWrapper({
+	authSelector: state => state.auth,
+  predicate: auth => auth.isAuthenticated,
+  redirectAction: history.push,
+  failureRedirectPath: '/login',
+  wrapperDisplayName: 'UserIsAuthenticated'
+});
 
 export default (
   <Route>
     <Route path="/" component={App}>
+    	<Route path='/dashboard' component={UserIsAuthenticated(Dashboard)} />
       <Route path="*" component={NotFound} />
     </Route>
   </Route>
