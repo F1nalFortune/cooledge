08b774c38ccf39bd75bea3b33c112c998953076c
sonicspeed123 <joshuasahagun1996@yahoo.com>
2016-09-26 19:13:23 -0600

    added image slider and dependance on react-materialize

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Register from './Register';
+import {Slider, Slide} from 'react-materialize';
 
 class Landing extends React.Component {
 	constructor(props){
@@ -26,13 +27,25 @@ class Landing extends React.Component {
 		}
 	}
 
+
 	render(){
 
 		return(
+
+
+
 			<div className='container'>
 				<div className='row'>
 					<h1 className='col s7 offset-s3 sitefont'>Cooledge</h1>
 					<p className="sitefont">For College Kids</p>
+					{/*Landing Image Slider*/}
+					<div className='col s12'>
+						<Slider>
+							<Slide title='this is title' src='https://upload.wikimedia.org/wikipedia/commons/d/dc/Two_Friends_Shaking_Hands.jpg'>
+								Make Friends
+							</Slide>
+						</Slider>
+					</div>
 				</div>
 				<div className='row'>
 					<button id='toggle' className='btn col s2 offset-s5 blue-grey' onClick={this.toggleForm}>Register now</button>
