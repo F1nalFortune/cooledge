2c6eaef99f1ab77ffc44af6e8a9689525425ab5b
Brian <juniortoad1738@gmail.com>
2016-10-12 20:00:13 -0600

    created about page

--- client/components/About.js
+++ client/components/About.js
@@ -12,58 +12,75 @@ class About extends React.Component{
 
   render(){
     return(
-      <div className='row bck about'>
+        <div className="bck">
+          <div id="ten" className="imageback">
+            <div className="z-depth-5">
+              <div className="container center">
+                <div className="row">
+                  <h1 className=" sitefont center white-text"> Meet the Team </h1>
 
-        <div className='col about-back s12 m8 offset-m2'>
-          <h1 className='col s12 center'>About the Team and App</h1>
-          <p className='col s12'>asdfasdfasdf</p>
+                  <div className="col s4">
+                    <div className="card-panel z-depth-3">
+                      <img id="image-id" className="hundred" src="/images/edwin.jpg" />
+                      <h5 className="card-title center crd">Edwin</h5>
+                      <span className="black-text">
+                        I have spent the last several years working in the field of video and audio post-production.
+                        I have been able to see the power of internet development and how it creates a connection
+                        with the user. I enjoy hiking, fishing, graphic art, traveling,and reading.
+                      </span>
+                      <br/>
+                      <a className="greenz fa fa-github fa-2x" href="https://github.com/edsant1" ></a>
+                      <a className="greenz fa fa-linkedin-square fa-2x" href="https://www.linkedin.com/in/edwinsantizo" ></a>
+                      <br/>
+                      <a className="greenz purpl buttonvw" href="sullivangln87@gmail.com">edwin@mia.com</a>
+                    </div>
+                  </div>
+
+
+
+                  <div className="col s4">
+                    <div className="card-panel white z-depth-3">
+                      <img id="image-id" className="fifty" src="/images/brian.jpg" />
+                      <h5 className="card-title center salt black-text crd">Brian Phelps</h5>
+                      <span className="black-text">
+                        Spending the last four years commited to graduating college, I wanted to continue my education. 
+                        After receiving a diploma with a focus on business marketing, I became reaquainted with computers. 
+                        I have been interested in them since I was very young, and wish to use the skills I earned at DevPoint 
+                        Labs in the field! I enjoy music, coding, playing sports, and learning.
+                      </span>
+                      <br/>
+                      <a className="greenz fa fa-github fa-2x" href="https://github.com/F1nalFortune"></a>
+                      <a className="greenz fa fa-linkedin-square fa-2x" href="https://www.linkedin.com/in/phelpsb92"></a>
+                      <br/>
+                      <a className="greenz purpl buttonvw" href="schristopherson@gmail.com">phelpsb92@gmail.com</a>
+                    </div>
+                  </div>
+
+                  <div className="col s4">
+                    <div className="card-panel z-depth-3">
+                      <img id="image-id" className="hundred" src="/images/josh.jpg" />
+                      <h5 className="card-title center salt crd">Josh</h5>
+                      <span className="black-text">
+                        Josh is always looking to improve, be it at coding or everyday life. He’s positive about most things 
+                        but can easily stress himself out.
+                      </span>
+                      <br/>
+                      <a className="greenz fa fa-github fa-2x" href="https://github.com/sonicspeed123"></a>
+                      <a className="greenz fa fa-linkedin-square fa-2x" href="https://www.linkedin.com/in/sahagunjoshua" ></a>
+                      <br/>
+                      <a className="greenz purpl buttonvw" href="lovelandnoah@gmail.com">joshuasahagun1996@yahoo.com</a>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
-        {/*
-          this is the start of each persons about.
-          Replace img src with your image
-          Be sure to replace the href with appropriate links.
-          Make sure that at the start of every set of 3, use the first template
-          templates 2 is for every second and third
-          (ex. template-1 template-2 temp-2 
-          temp-1 temp-2 temp-2)
-        */}
-
-        {/*template 1
-        <div className='row about-back col s12 m2 offset-m2'>
-
-          <img 
-            className='col s6 offset-s3 offset-m1 m10  about-pic'
-            src='http://vignette3.wikia.nocookie.net/blossom/images/d/da/Sonia90.jpg/revision/latest?cb=20131112131310'
-          />
-
-          <p className='col offset-s3 s6 offset-m1 m10'>
-            This is a paragraph about the you.
-          </p>
-
-          <i className="col m3 fa fa-github fa-2x" aria-hidden="true"></i><a href="#" className='about-link col m9 center'>Github</a>
-          <i className="col m3 fa fa-linkedin fa-2x" aria-hidden="true"></i><a href='#' className='about-link col m9 center'>LinkedIn</a>
-          <i className='col m3 fa fa-envelope fa-2x' aria-hidden='true'></i><a href='#' className='about-link col m9 center'>E-mail</a>
-        </div>
-        */}
 
         
-        {/*template 2
-        <div className='row about-back col s12 m2 offset-m1'>
-          <img 
-            className='col s6 offset-s3 offset-m1 m10  about-pic'
-            src='http://vignette3.wikia.nocookie.net/blossom/images/d/da/Sonia90.jpg/revision/latest?cb=20131112131310'
-          />
-          <p className='col offset-s3 s6 offset-m1 m10'>
-            This is a paragraph about the people. sdkasdads d dsfkasdf ds fds kdsa kdsak fs jkdfjkfsdjk
-          </p>
-          <i className="col m3 fa fa-github fa-2x" aria-hidden="true"></i><a href="#" className='about-link col m9 center'>Github</a>
-          <i className="col m3 fa fa-linkedin fa-2x" aria-hidden="true"></i><a href='#' className='about-link col m9 center'>LinkedIn</a>
-          <i className='col m3 fa fa-envelope fa-2x' aria-hidden='true'></i><a href='#' className='about-link col m9 center'>E-mail</a>
-        </div>
-        */}
 
-      </div>
     )
   }
 }
